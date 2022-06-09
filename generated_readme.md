48ea7f80153286d2a0f9ee69ed5dc36c13f3d369
Ricky Palmer <r.palmer@outlook.com>
2022-06-09 13:09:43 +0100

    made questions, changed readme format slightly

---
+++ generated_readme.md
@@ -0,0 +1,47 @@
+# Project Title ![MIT](https://img.shields.io/badge/MIT-License-green)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Getting Started](#getting-started)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Credits](#credits)
+
+
+## Description
+
+Lorem
+
+## Installation
+
+Please follow the instructions below:
+```
+
+```
+
+## Getting Started
+
+Please follow the instructions below:
+```
+
+```
+## License
+MIT license
+
+## Contributing
+lorem
+
+## Tests
+
+Please follow the instructions below:
+```
+npm run test
+```
+
+## Credits
+Please contact on my email: r.palmer@outlook.com
+
+Visit my github profile [here](https)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-console.log("Hello World");
+// required packages to make the app work 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./markdown");
+
+
+// array of questions for user
+const questions = [
+
+    {
+        type: "input",
+        message: "What is the project title?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message: "What is the project description?",
+        name: "description",
+    },
+
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installInstructions",
+    },
+
+    {
+        type: "input",
+        message: "How do you run the project?",
+        name: "gettingStarted",
+    },
+
+    {
+        type: "input",
+        message: "What are the contribution guidelines? ",
+        name: "guidelines",
+    },
+
+    {
+        type: "input",
+        message: "What are the test instructions? ",
+        name: "testInstructions",
+    },
+
+    {
+        type: "list",
+        message: "What is the license? ",
+        name: "license",
+        choices: ["MIT", "GNU v3.0", "Apache v2.0", "None"],
+    },
+
+    {
+        type: "input",
+        message: "What is your GitHub username? ",
+        name: "username",
+    },
+
+    {
+        type: "input",
+        message: "What is your email address? ",
+        name: "email",
+    },
+];
+
